a829e8c0f7addc763ce4a220bca0c423cd208b7c
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-04-16 14:18:20 -0400

    reset banana

--- Banana.java
+++ Banana.java
@@ -6,6 +6,20 @@ public class Banana extends Actor
     public void act()
     {
         // Add your action code here.
-        move(-3);
+        move(-10);
+        
+        if(getX() <= 0){
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600,100);
+
+        }else{
+            setLocation(600,300);
+        }
     }
 }
